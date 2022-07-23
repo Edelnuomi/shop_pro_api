acfd1435bd2543ba6e4210824e93f66ff0bae516
Edel <1916440204@qq.com>
2022-07-23 19:19:02 +0800

    第一次提交

---
+++ ship_Api/src/test/java/com/example/ship_api/ShipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ship_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
